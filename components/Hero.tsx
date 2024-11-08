d37b47f899ddf400c0f6bc6bd8830daacf7f096e
mhd_safwan <safwanpk2000@gmail.com>
2024-11-08 19:40:24 +0530

    first n3

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Hi I’m Safwan PK, a FULL Stack Developer creating dynamic web applications"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
