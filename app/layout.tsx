a3e3f08aacc04c0e4ebc6c4cfe2e9610cfe5d285
mhd_safwan <safwanpk2000@gmail.com>
2024-11-08 20:15:55 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link
+          rel="icon"
+          href="https://img.icons8.com/ios-filled/50/circled-s.png"
+          sizes="any"
+        />
       </head>
       <body className={inter.className}>
         <ThemeProvider
