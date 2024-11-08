078556fe54c437d04e561e59e7e1a7fcb0bc0d80
mhd_safwan <safwanpk2000@gmail.com>
2024-11-08 17:51:18 +0530

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <a
               key={info.id}
-              href={info.link}
+              // href={info.link}
               target="_blank"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
