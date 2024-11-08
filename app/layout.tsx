8a9d13dd98f152406268c921f92919692364f09c
mhd_safwan <safwanpk2000@gmail.com>
2024-11-08 19:26:54 +0530

    hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "safwan pk's Portfolio",
+  description: "MERN Stack ",
 };
 
 export default function RootLayout({
